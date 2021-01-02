e6058c49cd48d34e9f0aafbeb1d86163e7e009e1
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-02 22:58:21 +0530

    Create firstday.py

---
+++ firstday.py
@@ -0,0 +1,17 @@
+# **Remove element from first position and last position from list..**
+
+
+l1 = [34,'svkm',15,'s',21] 
+print("given list =",l1) 
+print("Remove first position") 
+l1.remove(l1[0]) 
+print(l1) 
+print("remove last location") 
+l3 = [34,'svkm',15,'s',21] 
+l3.remove(l3[-1]) 
+print(l3)
+
+
+
+
+
