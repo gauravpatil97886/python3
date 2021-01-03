51e63e5088579a021a00e4cffdc990d4b1e7ebf7
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-03 22:55:30 +0530

    Update thirdday.py

--- thirdday.py
+++ thirdday.py
@@ -28,4 +28,20 @@ for x in a:
 print(list1)
 
 
+# Write a  program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
+
+# Sample List : ['abc', 'xyz', 'aba', '1221']
+lst = []
+n = int(input("length of a list: "))
+print("Enter list elements: ")
+count = 0;
+for i in range(n):
+    l1 = input()
+    lst += [l1]
+    if len(l1) > 1 and l1[0] == l1[-1]:
+        count = count + 1
+print("Given list is : ",lst)
+print("number of strings having same first and last character in a list : ",count)
+
+
 
