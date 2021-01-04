1c3b2f594942722b64218ab1b7a7d96a97a7f810
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-04 22:25:38 +0530

    Create 4day.py

---
+++ 4day.py
@@ -0,0 +1,14 @@
+# python3 @python3
+list1 = [10, 21, 4, 45, 66, 93,11,22,33,45,65,44,78,34,7,8] 
+print(" numbers of a specified list after removing even numbers from it. aGet input from user for every program")  
+
+for num in list1: 
+      
+   
+    if num % 2 == 0:
+      print(num, end = " ")
+      
+      
+      
+      
+      
