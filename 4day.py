95a2edc5ce0ae19ec0343b456082fc3c35a712b5
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-04 22:32:32 +0530

    Update 4day.py

--- 4day.py
+++ 4day.py
@@ -1,4 +1,9 @@
 # python3 @python3
+#Lists are used to store multiple items in a single variable.
+
+#Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+
+#Lists are created using square brackets:
 
 ----------------------
 -  #numbers of a specified list after removing even numbers from it. aGet input from user for every program
@@ -37,7 +42,7 @@ if not l:
 else:
     print ("list is not empty")
       
-      
+ -----------------------
       
       
       
