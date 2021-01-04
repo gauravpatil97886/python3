7a442fe56c1bc619fb3e38e8bd2a49c37eaa7b53
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-04 22:26:15 +0530

    Update 4day.py

--- 4day.py
+++ 4day.py
@@ -9,6 +9,20 @@ for num in list1:
       print(num, end = " ")
       
       
+
+      
+      
+      
+ str=input("enter strings : ")
+a=str.split(" ")
+b=[]
+n=int(input("enter value of n "))
+for i in a:
+   if (len(i)> n) :
+      b.append(i)
+print("list of words : ",b)
+      
+      
       
       
       
