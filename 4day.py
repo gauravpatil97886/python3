aa9858a1f026e9c2c63937d3c0dbf28f8be311f9
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-04 22:30:58 +0530

    Update 4day.py

--- 4day.py
+++ 4day.py
@@ -1,4 +1,8 @@
 # python3 @python3
+
+----------------------
+-  #numbers of a specified list after removing even numbers from it. aGet input from user for every program
+-------------------------
 list1 = [10, 21, 4, 45, 66, 93,11,22,33,45,65,44,78,34,7,8] 
 print(" numbers of a specified list after removing even numbers from it. aGet input from user for every program")  
 
@@ -10,9 +14,9 @@ for num in list1:
       
       
 
-      
-      
-      
+ ------------------------------     
+ - #program to find the list of words that are longer than n from a given list of words     
+ ------------------------------     
  str=input("enter strings : ")
 a=str.split(" ")
 b=[]
@@ -23,13 +27,9 @@ for i in a:
 print("list of words : ",b)
 
 
-
-
-
-
-
-
-
+-------------------------
+- #check list is empty or not
+--------------------------
 
 l = [3,3,3,3]
 if not l:
