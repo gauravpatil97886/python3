0ba1a37738fd5cfa111ec6a7a07cdb3be86c2bda
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-05 21:37:02 +0530

    Create 5day.py

---
+++ 5day.py
@@ -0,0 +1,12 @@
+#python @python
+
+## diffrence between two list
+
+list1 = [1, 2, 4,7,8,9]
+list2 = [4, 5, 6,5,3,2,1]
+
+a = set(list1) - set(list2)
+b = list(a)
+
+print(b)
+---------------------------------------------------------------------
