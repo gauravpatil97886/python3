79b213ff2d4cfd146d7c3655087a5fe22884261d
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-05 21:38:26 +0530

    Update 5day.py

--- 5day.py
+++ 5day.py
@@ -1,5 +1,7 @@
 #python @python
 
+
+-------------------------------------
 ## diffrence between two list
 
 list1 = [1, 2, 4,7,8,9]
@@ -10,3 +12,37 @@ b = list(a)
 
 print(b)
 ---------------------------------------------------------------------
+
+## Convert or join list to string..
+
+a = ['G' 'a' 'u' 'r' 'v']
+b = ['p', 'a', 't', 'i' 'l']
+c = ['svkm', 'iot', 'dhule', '424001']
+a = ['G' 'a' 'u' 'r' 'v']
+b = ['p', 'a', 't', 'i' 'l']
+c = ['svkm', 'iot', 'dhule', '424001']
+a = ['G' 'a' 'u' 'r' 'v']
+b = ['p', 'a', 't', 'i' 'l']
+c = ['svkm', 'iot', 'dhule', '424001']
+str1 = ''.join(a)
+str2 = ''.join(b)
+str3 = ''.join(c)
+str1 = ''.join(a)
+str2 = ''.join(b)
+str3 = ''.join(c)
+print(str1)
+print(str2)
+print(str3)
+print(str1)
+print(str2)
+print(str3)
+----------------------------------------------------
+
+
+
+
+
+
+
+
+
