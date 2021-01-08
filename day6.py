1e430cb620012b0a7a50a8d949be8f56f14110c6
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-08 21:49:23 +0530

    Update day6.py

--- day6.py
+++ day6.py
@@ -9,3 +9,25 @@ my_new_list = list(my_set)
 print("List of unique numbers : ",my_new_list)
 
 ------------------------------------------------------
+
+# Write a  program to check whether two lists are circularly identical.
+
+A=[]
+n=int(input("length of list:"))
+print("elements of first list:")
+for i in range(int(n)):
+   k=int(input(""))
+   A.append(k)
+
+B=[]
+n1=int(input("length of list:"))
+print("Element of the Second List:")
+for i in range(int(n1)):
+   k=int(input(""))
+   B.append(k)
+if ' '.join(map(str, B)) in ' '.join(map(str, A * 2)):
+   print("lists are circularly identical")
+else:
+    print("lists are not circularly identical")
+
+    --------------------------------------------
