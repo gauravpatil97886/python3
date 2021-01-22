3702b99f3fe16dcbf5885b66db5f3c6b25265068
Gaurav Patil <63364646+gauravpatil97886@users.noreply.github.com>
2021-01-22 21:20:25 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -11,3 +11,6 @@ result = list(map(lambda x: 2 ** x, range(terms)))
 print("The total terms are:",terms)
 for i in range(terms):
    print("2 raised to power",i,"is",result[i])
+
+ -------------------------
+
